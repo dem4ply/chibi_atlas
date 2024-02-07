413524486105ec4c0f764b33987fb8edeac551cf
dem4ply <dem4ply@gmail.com>
2024-02-06 23:22:37 -0600

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -30,6 +30,6 @@ setup(
     name='chibi_atlas',
     packages=find_packages(include=['chibi_atlas', 'chibi_atlas.*']),
     url='https://github.com/dem4ply/chibi_atlas',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
